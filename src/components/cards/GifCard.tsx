de68f295945ea94bd49931a4baa07ffca754e539
Muhammad Salman <dev.iamsalman@gmail.com>
2021-07-05 01:58:15 +0500

    adds GifCard

--- src/components/cards/GifCard.tsx
+++ src/components/cards/GifCard.tsx
@@ -1,4 +1,17 @@
 import Box from "@material-ui/core/Box"
-export const GifCard: React.FC<{}> = () => {
-    return <Box></Box>
+import { LazyLoadWrapper } from '~/components'
+
+interface Props {
+    item: any;
+}
+export const GifCardDimension = { height: "320px", width: "280px" }
+
+export const GifCard: React.FC<Props> = ({ item }) => {
+
+
+    return <Box position="relative" display="flex" borderRadius="4px" boxShadow="0 0 2px lightgrey" {...GifCardDimension}>
+        <LazyLoadWrapper>
+            {({ onLoad }) => <img onLoad={onLoad} height="100%" width="100%" src={item?.images?.fixed_height_small?.webp} alt="loading..." />}
+        </LazyLoadWrapper>
+    </Box>
 }
