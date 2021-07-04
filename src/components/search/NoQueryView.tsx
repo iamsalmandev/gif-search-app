6fe07744ceccbbb9a2862d9081cd560ff45d55bb
Muhammad Salman <dev.iamsalman@gmail.com>
2021-07-05 02:23:02 +0500

    adds NoQueryView

---
+++ src/components/search/NoQueryView.tsx
@@ -0,0 +1,10 @@
+import Box from "@material-ui/core/Box"
+
+export const NoQueryView = () => {
+
+    return <Box display="flex" justifyContent="center" alignItems="center" width="100%" height="250px" pb="100px" >
+        <Box component="h4">
+            You'll See Search Results here
+        </Box>
+    </Box>
+}
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import { SearchHeader, SearchHeaderProps } from './SearchHeader'
 import { SearchResults } from './SearchResults'
 import { useTrendingGifySearch } from '~/hooks'
-
+import { NoQueryView } from './NoQueryView'
 export const Search = () => {
     const [text, setText] = useState<SearchHeaderProps['text']>('');
     const [order, setOrder] = useState<SearchHeaderProps['order']>('ASC');
     const { loading, data, loadMore } = useTrendingGifySearch({ query: text })
-    console.log("loading", loading)
     return <>
         <SearchHeader setText={setText} text={text} order={order} setOrder={setOrder} />
+        {!data.length && <NoQueryView />}
         <SearchResults onScrollEnd={loadMore} loading={loading} data={data} />
     </>
 }
