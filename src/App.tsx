7a9d57c154ff535cd9ee85c38eae5c58638113a5
Muhammad Salman <dev.iamsalman@gmail.com>
2021-07-05 02:05:00 +0500

    adds changes on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-
+import Box from "@material-ui/core/Box"
+import { Header, Search, Footer } from '~/components'
+import { HEADER_HEIGHT } from '~/constants'
 
 const App: React.FC<{}> = () => {
   return (
     <div >
+      <Header />
+      <Box marginTop={`${HEADER_HEIGHT}px`}>
+        <Search />
+        <Footer />
+      </Box>
     </div>
   );
 }
